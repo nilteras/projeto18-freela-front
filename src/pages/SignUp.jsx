471c205ef05b9600352d0b0ea511bfdc34cf5b24
nilteras <niltonrnascimento@hotmail.com>
2023-08-08 21:09:29 -0300

    feat: SignUpPage config layout

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,103 @@
-export default function SignUp(){
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import styled from 'styled-components'
+
+export default function SignUp() {
+
+    const [name, setName] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [phone, setPhone] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    function createUser(e) {
+        e.preventDefault()
+        const body = {
+            name,
+            cpf,
+            phone,
+            email,
+            password,
+            confirmPassword
+        }
+        console.log(body)
+    }
+
     return (
         <>
-        CAFDASTRO
+            <SingUpContainer>
+                <form onSubmit={createUser}>
+
+                    <input
+                        name="name"
+                        placeholder="Nome"
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required
+                    />
+
+                    <input
+                        name="cpf"
+                        placeholder="CPF"
+                        type="text"
+                        value={cpf}
+                        onChange={e => setCpf(e.target.value)}
+                        required
+                    />
+
+                    <input
+                        name="phone"
+                        placeholder="Número de Telefone"
+                        type="text"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                        required
+                    />
+                    <input
+                        name="email"
+                        placeholder="E-mail"
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                    />
+                    <input
+
+                        name="password"
+                        placeholder="Senha"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+
+
+                    />
+                    <input
+
+                        name="confirmPassword"
+                        placeholder="Confirme a senha"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={e => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                    <button type="submit">Cadastrar</button>
+                </form>
+
+                <Link to={"/"}>
+                    Já tem uma conta? Entre agora!
+                </Link>
+            </SingUpContainer>
         </>
     )
 }
+
+const SingUpContainer = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
