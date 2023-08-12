e8c6fd43c9e1772e59153b16a03e49ba09940724
nilteras <niltonrnascimento@hotmail.com>
2023-08-12 12:33:58 -0300

    feat: Connection API route manager post

--- src/pages/ManagePost.jsx
+++ src/pages/ManagePost.jsx
@@ -2,13 +2,17 @@ import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../context/UserContext"
 import axios from "axios"
 import styled from 'styled-components'
+import { Link, useNavigate } from "react-router-dom"
 
-export default function ManagePost(){
+export default function ManagePost() {
 
     const [postInfo, setPostInfo] = useState([])
     const BaseURL = import.meta.env.VITE_API_URL
     const { user } = useContext(UserContext)
     const { id, name } = user
+    const [availability, setAvailability] = useState({})
+    const navigate = useNavigate()
+
     useEffect(() => {
         const promise = axios.get(`${BaseURL}/posts/edit/${id}`)
         promise.then(res => {
@@ -18,24 +22,61 @@ export default function ManagePost(){
             console.log(err.message)
         })
     }, [])
-    console.log(postInfo)
+
+    function saveChanges() {
+        postInfo.forEach((p) => {
+            const body = {
+                id: p.id,
+                available: availability[p.id] || false
+            };
+
+            const promise = axios.put(`${BaseURL}/posts/edit`, body);
+            promise.then(res => {
+                console.log(`Post ${p.id} atualizado`);
+            });
+            promise.catch(err => {
+                console.log(`Erro ao atualizar post ${p.id}:`, err.message);
+            });
+            navigate('/home')
+        });
+    }
+
+
+
     return (
         <>
-         <HomeContainer>
+            <HomeContainer>
                 <TransactionsContainer>
                     <h1> DOGSTAR </h1>
                     <h1>Olá, {user.name}</h1>
-                    
+
                     {postInfo.map((p) => (
                         <ListItemContainer key={p.id}>
                             <div>
                                 <img src={p.image} alt='' />
                                 <p>Nome: {p.name_dog} </p>
                                 <p>Talentos: {p.description}</p>
+                                <label>
+                                    Disponível:
+                                    <input
+                                        type="checkbox"
+                                        checked={availability[p.id] || false}
+                                        onChange={(e) => {
+                                            setAvailability({
+                                                ...availability,
+                                                [p.id]: e.target.checked
+                                            });
+                                        }}
+                                    />
+                                </label>
                             </div>
+
                         </ListItemContainer>
                     ))}
-
+                    <BotaoADD onClick={saveChanges}>
+                        <p>Salvar alterações</p>
+                    </BotaoADD>
+                   
                 </TransactionsContainer>
             </HomeContainer>
         </>
@@ -82,4 +123,11 @@ const ListItemContainer = styled.li`
     height: 60px;
   }
   cursor: pointer;
+`
+
+export const BotaoADD = styled.div`
+    background-color: white;
+    width: 180px;
+    height: 30px;
+    cursor: pointer;
 `
